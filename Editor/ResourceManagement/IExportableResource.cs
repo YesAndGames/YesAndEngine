5a27b51e8c2e90969a25628a861bd5fc4919e976
Weslo <wesley.rockholz@gmail.com>
2016-09-26 14:18:21 -0400

    add exportable resource interface

---
+++ Editor/ResourceManagement/IExportableResource.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace YesAndEditor.Exporting {
+
+	// An exportable resource interface.
+	public interface IExportableResource<T> {
+
+		// Serialize the data into exportable format.
+		IExportableResource<T> Serialize (T data);
+	}
+}
