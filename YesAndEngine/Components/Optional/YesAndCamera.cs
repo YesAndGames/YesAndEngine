3167a100e02d22dcf10f83b573140e1bd7ade942
Weslo <wesley.rockholz@gmail.com>
2016-11-01 17:42:31 -0400

    Add input listener and Yes And Camera components

---
+++ YesAndEngine/Components/Optional/YesAndCamera.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace YesAndEngine.Components {
+
+	// An extension of the Unity Camera component that adds functionality and controls.
+	public class YesAndCamera : MonoBehaviour {
+
+		// The possible control states for a camera.
+		public enum CameraControlMode {
+			Locked,
+			Unlocked,
+			Following,
+		}
+
+		// The YesAndCamera attached to Camera.main.
+		public static YesAndCamera main { get; private set; }
+
+		// The current mode of this camera.
+		public CameraControlMode Mode { get; private set; }
+
+		// The dampening for velocity.
+		[SerializeField, Range (0f, 1f)]
+		private float damping = 0.9f;
+
+		// The position the camera should be moving towards.
+		private Vector3 targetPosition;
+
+		// The currenty velocity of this camera.
+		private Vector3 velocity;
+
+		// The list of targets currently being followed.
+		private List<Transform> following;
+
+		// Initialize this component.
+		void Awake () {
+			main = Camera.main.GetComponent<YesAndCamera> ();
+			following = new List<Transform> ();
+			Lock ();
+		}
+
+		// Update this component.
+		void Update () {
+			switch (Mode) {
+			case CameraControlMode.Locked:
+				break;
+			case CameraControlMode.Unlocked:
+				velocity.z = 0;
+				transform.Translate (velocity);
+				break;
+			case CameraControlMode.Following:
+				break;
+			}
+		}
+
+		// Update this component at fixed intervals.
+		void FixedUpdate () {
+			velocity *= damping;
+		}
+
+		// Lock this camera to any controls.
+		public void Lock () {
+			Reset ();
+			Mode = CameraControlMode.Locked;
+		}
+
+		// Unlock this camera to scripted controls.
+		public void Unlock () {
+			Reset ();
+			Mode = CameraControlMode.Unlocked;
+		}
+
+		// Begin following the list of targets.
+		public void Follow (params Transform[] targets) {
+			Reset ();
+			following.AddRange (targets);
+			Mode = CameraControlMode.Following;
+		}
+
+		// Drag this camera for a frame.
+		public void Drag (PointerEventData pointer) {
+			velocity = -(Camera.main.ScreenToWorldPoint (pointer.position) - Camera.main.ScreenToWorldPoint (pointer.position - pointer.delta));
+		}
+
+		// Reset fields between states.
+		private void Reset () {
+			targetPosition = transform.position;
+			velocity = Vector3.zero;
+			following.Clear ();
+		}
+	}
+}
---
+++ YesAndEngine/Input/InputListener.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+// Place this behavior on a GUI rect transform to listen for input on that space.
+public class InputListener : MonoBehaviour,
+	IPointerDownHandler,
+	IPointerUpHandler,
+	IPointerClickHandler,
+	IDragHandler {
+
+	// An event delegate with a PointerEventData argument.
+	[Serializable]
+	public class PointerEvent : UnityEvent<PointerEventData> { }
+
+	// An event delegate with two Touch arguments.
+	[Serializable]
+	public class TwoTouchEvent : UnityEvent<Touch, Touch> { }
+
+	// Fires on the frame a pointer goes down.
+	[SerializeField]
+	public PointerEvent PointerDown;
+
+	// Fires on the frame a pointer goes up.
+	[SerializeField]
+	public PointerEvent PointerUp;
+
+	// Fires on the frame a click or tap occurs.
+	[SerializeField]
+	public PointerEvent PointerClick;
+
+	// Fires each frame the listener interprets a drag gesture.
+	[SerializeField]
+	public PointerEvent Drag;
+
+	// Fires each frame the listener interprets a pinch or inverse pinch gesture.
+	[SerializeField]
+	public TwoTouchEvent Pinch;
+
+	// Clean up this event listener and detach events.
+	public void Dispose () {
+		if (Drag != null) {
+			Drag.RemoveAllListeners ();
+		}
+		if (Pinch != null) {
+			Pinch.RemoveAllListeners ();
+		}
+	}
+
+	// Interpret a pointer down event.
+	public void OnPointerDown (PointerEventData eventData) {
+		if (PointerDown != null) {
+			PointerDown.Invoke (eventData);
+		}
+		UpdateTouchInput ();
+	}
+
+	// Interpret a pointer up event.
+	public void OnPointerUp (PointerEventData eventData) {
+		if (PointerUp != null) {
+			PointerUp.Invoke (eventData);
+		}
+		UpdateTouchInput ();
+	}
+
+	// Interpret a pointer cllick event.
+	public void OnPointerClick (PointerEventData eventData) {
+		if (PointerClick != null) {
+			PointerClick.Invoke (eventData);
+		}
+		UpdateTouchInput ();
+	}
+
+	// Interpret a drag event.
+	public void OnDrag (PointerEventData eventData) {
+		if (Drag != null) {
+			Drag.Invoke (eventData);
+		}
+		UpdateTouchInput ();
+	}
+
+	// Interpret touch input that isn't fired by Unity input events.
+	private void UpdateTouchInput () {
+
+		// If there is at least one touch on the device...
+		if (Input.touchCount > 0) {
+			Touch touchZero = Input.GetTouch (0);
+
+			// If there are two touches on the device...
+			if (Input.touchCount == 2) {
+				Touch touchOne = Input.GetTouch (1);
+
+				// Fire a pinch event.
+				if (Pinch != null) {
+					Pinch.Invoke (touchZero, touchOne);
+				}
+			}
+		}
+	}
+}
