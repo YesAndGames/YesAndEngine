c61093c77b2c982689e0fd69db3a62a881c72125
Weslo <wesley.rockholz@gmail.com>
2016-11-03 09:07:41 -0400

    Add more functionality to YesAndCamera

--- YesAndEngine/Components/Optional/YesAndCamera.cs
+++ YesAndEngine/Components/Optional/YesAndCamera.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -14,11 +15,23 @@ namespace YesAndEngine.Components {
 			Following,
 		}
 
+		// Constants.
+		private const float REACHED_TARGET_THRESHOLD = 0.1f;
+
 		// The YesAndCamera attached to Camera.main.
 		public static YesAndCamera main { get; private set; }
 
 		// The current mode of this camera.
 		public CameraControlMode Mode { get; private set; }
+
+		// If set to true, this camera is restricted to motion inside its boundaries.
+		public bool LockedToBounds { get; set; }
+
+		// If set to true, this camera is allowed to move along the horizontal axis.
+		public bool HorizontalMotionEnabled { get; set; }
+
+		// If set to true, this camera is allowed to move along the vertical axis.
+		public bool VerticalMotionEnabled { get; set; }
 
 		// The dampening for velocity.
 		[SerializeField, Range (0f, 1f), Tooltip ("Rate of momentum damping.")]
@@ -36,17 +49,30 @@ namespace YesAndEngine.Components {
 		[SerializeField, Tooltip ("Maximum orthographic camera size.")]
 		private float maxOrthoSize = 10f;
 
+		// Position bounds.
+		private float? leftBound;
+		private float? rightBound;
+		private float? topBound;
+		private float? bottomBound;
+
 		// The camera attached to this camera.
 		private Camera cam;
 
 		// The position the camera should be moving towards.
-		private Vector3 targetPosition;
+		private Vector3? targetPosition;
+
+		// The orthographic size the camera should be moving towards.
+		private float? targetSize;
 
 		// The currenty velocity of this camera.
 		private Vector3 velocity;
 
 		// The list of targets currently being followed.
 		private List<Transform> following;
+
+		// Camera shake variables.
+		private float shakePower;
+		private float shakeDuration;
 
 		// Initialize this component.
 		void Awake () {
@@ -58,17 +84,32 @@ namespace YesAndEngine.Components {
 
 		// Update this component.
 		void Update () {
+
 			switch (Mode) {
 			case CameraControlMode.Locked:
 				break;
 			case CameraControlMode.Unlocked:
-				velocity.z = 0;
+				RestrictVelocity ();
 				transform.Translate (velocity);
 				velocity *= Mathf.Pow (damping, Time.deltaTime);
 				break;
 			case CameraControlMode.Following:
 				break;
 			}
+
+			// Update automatic camera motion.
+			if (targetPosition != null) {
+				Vector3 tar = (Vector3)targetPosition;
+				velocity = tar - transform.position;
+				RestrictVelocity ();
+				transform.Translate (velocity);
+				if (Vector3.Distance (tar, transform.position) < REACHED_TARGET_THRESHOLD) {
+					targetPosition = null;
+				}
+			}
+
+			// Clamp to bounds.
+			ClampPosition ();
 		}
 
 		// Lock this camera to any controls.
@@ -88,6 +129,38 @@ namespace YesAndEngine.Components {
 			Reset ();
 			following.AddRange (targets);
 			Mode = CameraControlMode.Following;
+		}
+
+		// Restrict to the specified bounds. Any bounds assigned to null will be ignored
+		public void SetBounds (float? left = null, float? right = null, float? top = null, float? bottom = null) {
+			leftBound = left;
+			rightBound = right;
+			topBound = top;
+			bottomBound = bottom;
+			LockedToBounds = true;
+			HorizontalMotionEnabled = left != null || right != null;
+			VerticalMotionEnabled = top != null || bottom != null;
+		}
+
+		// Release from all bounds.
+		public void ReleaseBounds () {
+			SetBounds (null, null, null, null);
+			LockedToBounds = false;
+			HorizontalMotionEnabled = true;
+			VerticalMotionEnabled = true;
+		}
+
+		// Translate the camera by a vector.
+		public void Translate (Vector2 translation) {
+			transform.Translate (translation);
+		}
+
+		// Target the specified position, move there but do not follow.
+		public void MoveTo (Vector2 target, float? size = null) {
+			targetPosition = target;
+			if (size != null) {
+				targetSize = (float)size;
+			}
 		}
 
 		// Drag this camera for a frame.
@@ -129,10 +202,101 @@ namespace YesAndEngine.Components {
 		}
 
 		// Reset fields between states.
-		private void Reset () {
-			targetPosition = transform.position;
+		public void Reset (Vector3? position = null, float? size = null, Color? clearColor = null) {
+
+			// Reset the Camera component.
+			if (position != null) {
+				transform.position = (Vector3)position;
+			}
+			if (size != null) {
+				cam.orthographicSize = (float)size;
+			}
+			if (clearColor != null) {
+				cam.backgroundColor = (Color)clearColor;
+			}
+
+			// Reset fields.
+			Mode = CameraControlMode.Unlocked;
+			ReleaseBounds ();
+			targetPosition = null;
+			targetSize = null;
 			velocity = Vector3.zero;
 			following.Clear ();
+		}
+
+		// Shake this camera.
+		public void Shake (float power, float duration) {
+			shakePower = power;
+			shakeDuration = duration;
+			StartCoroutine (Shaking ());
+		}
+
+		// Coroutine for camera shake.
+		private IEnumerator Shaking () {
+
+			float elapsed = 0.0f;
+			Vector3 preShakePos = cam.transform.position;
+
+			while (elapsed < shakeDuration) {
+
+				elapsed += Time.deltaTime;
+
+				float percentComplete = elapsed / shakeDuration;
+				float damper = 1.0f - Mathf.Clamp (4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+				// map value to [-1, 1]
+				float x = Random.value * 2.0f - 1.0f;
+				float y = Random.value * 2.0f - 1.0f;
+
+				x *= shakePower * damper;
+				y *= shakePower * damper;
+
+				x += preShakePos.x;
+				y += preShakePos.y;
+
+				transform.localPosition = new Vector3 (x, y, transform.position.y);
+
+				yield return null;
+			}
+
+			transform.position = preShakePos;
+		}
+
+		// Restrict the current velocity given all the bounds.
+		private void RestrictVelocity () {
+			velocity.z = 0;
+
+			if (!HorizontalMotionEnabled) {
+				velocity.x = 0;
+			}
+			if (!VerticalMotionEnabled) {
+				velocity.y = 0;
+			}
+		}
+
+		// Clamp the current camera position.
+		private void ClampPosition () {
+
+			// Clamp bounds if they exist.
+			float x = transform.position.x, y = transform.position.y;
+			if (leftBound != null) {
+				float l = (float)leftBound;
+				x = x < l ? l : x;
+			}
+			if (rightBound != null) {
+				float r = (float)rightBound;
+				x = x > r ? r : x;
+			}
+			if (topBound != null) {
+				float t = (float)topBound;
+				y = y > t ? t : y;
+			}
+			if (bottomBound != null) {
+				float b = (float)bottomBound;
+				y = y < b ? b : y;
+			}
+
+			transform.position = new Vector3 (x, y, transform.position.z);
 		}
 	}
 }
