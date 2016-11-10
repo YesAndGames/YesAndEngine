f701bf924ac24106a9ae4aee2b09503ebc90b0d6
Weslo <wesley.rockholz@gmail.com>
2016-11-09 23:00:30 -0500

    Add data structures for weighted data collections

---
+++ YesAndEngine/DataStructures/Optional/WeightedCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace YesAndEngine.DataStructures {
+
+	// A collection of weighted objects.
+	[Serializable]
+	public class WeightedCollection<T> : WeightedObject where T : WeightedObject {
+
+		// The array of weighted objects.
+		public T[] Objects {
+			get {
+				return objects;
+			}
+		}
+		[SerializeField]
+		private T[] objects;
+
+		// Construct a new WeightedCollection object with no data.
+		public WeightedCollection () {
+			objects = new T[0];
+		}
+
+		// Construct a new WeightedCollection object.
+		public WeightedCollection (T[] objects, int weight = 1) : base (weight) {
+			this.objects = objects;
+		}
+
+		// Calculates the total weight in this collection.
+		public int GetTotalWeight () {
+			int totalWeight = 0;
+			for (int i = 0; i < objects.Length; i++) {
+				totalWeight += objects[i].Weight;
+			}
+			return totalWeight;
+		}
+	}
+}
---
+++ YesAndEngine/DataStructures/Optional/WeightedObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+namespace YesAndEngine.DataStructures {
+
+	// An object with a "weight", used for chanced pooling.
+	[Serializable]
+	public class WeightedObject {
+
+		// Integer weight, used to pick by weighted chance.
+		public int Weight {
+			get {
+				return weight;
+			}
+		}
+		[SerializeField]
+		private int weight;
+
+		// Construct a new WeightedObject object.
+		protected WeightedObject (int weight = 1) {
+			this.weight = weight;
+		}
+	}
+}
